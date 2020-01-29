9d0bbc9e4d55d2c5e47b0223a621ad16c03837a9
lucas_inestal <lucasainestal@gmail.com>
2020-01-29 13:53:03 +0100

    error handling if email already exists

--- Classes/register.php
+++ Classes/register.php
@@ -2,15 +2,6 @@
 
 include_once 'connection.php';
 
-  
-
-//importera connection, klart
-//Kolla mot inputs klart
-//Posta till DB 
-//Hasha lösenord
-//trycka in nu användare
-
-
 class Register{
     private $conn;
     private $table_name ="users";
@@ -26,29 +17,36 @@ class Register{
        public function register(){
            //hashing password
             $hash = password_hash($this->password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO users (username, password, email) VALUES ('$this->username', '$hash', '$this->email')";
 
-            $stmt = $this->conn->prepare($query);
-
-             // sanitize
-            $this->username=htmlspecialchars(strip_tags($this->username));
-            $this->password=htmlspecialchars(strip_tags($this->password));
-            $this->email=htmlspecialchars(strip_tags($this->email));
-
-            $stmt->bindParam(':username', $this->username);
-            $stmt->bindParam(':password', $this->password);
-            $stmt->bindParam(':email', $this->email);
-
-            if($stmt->execute()){
-                return true;
-            }else{
-                $this->showError($stmt);
-                return false;
+            //checking wether or not email already exists
+            $this->email;
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email='$this->email'");
+            $stmt->execute([$this->email]); 
+            $exists = $stmt->fetch();
+            if ($exists){
+                echo "user exists";
             }
+            else {
+                //saving to DB
+                $query = "INSERT INTO users (username, password, email) VALUES ('$this->username', '$hash', '$this->email')";
+
+                $stmt = $this->conn->prepare($query);
+
+                // sanitize
+                $this->username=htmlspecialchars(strip_tags($this->username));
+                $this->password=htmlspecialchars(strip_tags($this->password));
+                $this->email=htmlspecialchars(strip_tags($this->email));
+
+                if($stmt->execute()){
+                    return true;
+                }else{
+                    $this->showError($stmt);
+                    return false;
+                }
         }
+    }
 }
 
 $database = new Connection();
 $db = $database->openConnection();
 
-$user = new Register($db);
