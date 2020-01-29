015f2dac258d6425aea1952e2e35da6b87c59496
lucas_inestal <lucasainestal@gmail.com>
2020-01-29 09:35:59 +0100

    files

---
+++ Classes/register.php
@@ -0,0 +1,24 @@
+<?php
+
+//importera connection, 
+//Kolla mot inputs
+//Hämta från DB
+//Hasha lösenord
+//trycka in nu användare
+
+class Users{
+    public $id;
+    public $username;
+    public $password;
+    public $email;
+
+    public function hashPasswords(){
+        
+    }
+
+    public function createUser(){
+        
+    }
+  
+
+}
