e090537a8b8c795247de45b88ba0fe8315832742
Kevin Fridman <kevin.fridman@yh.nackademin.se>
2020-01-29 14:45:31 +0100

    fixed login

---
+++ Classes/login.php
@@ -0,0 +1,49 @@
+<?php
+//inkludera connection
+
+include_once "connection.php";
+
+class Login{
+    private $conn;
+
+    
+    public $password;
+    public $email;
+
+    public function __construct($db){
+        $this->conn = $db;
+
+}       
+ //Login user
+public function login(){
+   // echo("kommer in i get USER");
+   // session_start();
+    //$this->email = $_SESSION['email'];
+    //$this->password = $_SESSION['password'];
+
+    echo($this->email) . PHP_EOL;
+    echo($this->password);
+
+    //JÄMFÖRA MED DATABAS
+
+    $stmt = $this->conn->prepare("SELECT * FROM users WHERE email='$this->email'");
+    $stmt->execute([$this->email]); 
+    $match = $stmt->fetch();
+            
+    if ($match){
+        $user = json_encode($match);
+        print_r($user->password);
+        
+         //echo "user match";
+     }
+     else{
+                return false;
+            } 
+        }
+}
+
+$database = new Connection();
+$db = $database->openConnection();
+
+
+
--- login.php
+++ login.php
@@ -1,3 +1,41 @@
+
+<?php
+//session_start();
+// core configuration
+//include_once "config/core.php";
+
+// set page title
+
+// include login checker
+//include_once "login_checker.php";
+
+// include classes
+include_once 'Classes/connection.php';
+include_once 'Classes/login.php';
+//include_once "libs/php/utils.php";
+$loginUser= new Login($db);
+// include page header HTML
+//include_once "layout_head.php";
+if($_POST){
+
+    $database = new Connection();
+    $db = $database->openConnection();
+
+    $loginUser->email=$_POST['email'];
+    $loginUser->password=$_POST['password'];
+    $loginUser->login();
+}
+
+
+
+/*if(!empty($_POST['email']) && !empty($_POST['password'])) {
+    $loginUser->getUser($_POST['email'], $_POST['password']);
+}*/
+
+   ?> 
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,7 +44,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+
     <title>Login</title>
 </head>
 <body>
@@ -18,14 +56,12 @@
             <img id="profile-img" class="rounded-circle profile-img-card" src="https://i.imgur.com/6b6psnA.png" />
             <p id="profile-name" class="profile-name-card"></p>
 
-    <form action="/action_page.php">
-         <input type="text" name="name" placeholder="username"><br>
+    <form action="login.php" method="POST">
+         <input type="email" name="email" placeholder="email"><br>
          <input type="password" name="password" placeholder="password"><br>
          <br>
-        <input type="submit" value="Login" button class="btn btn-lg btn-primary btn-block btn-signin">
+        <input type="submit" value="submit" button class="btn btn-lg btn-primary btn-block btn-signin">
     </form>
 </body>
 </html>
 
-<?php
-?>
