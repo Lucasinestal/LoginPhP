03618e6d05e5d8a2e304f0b69738c91d525975fb
Patrik Johansson <patrik.johansson@yh.nackademin.se>
2020-01-30 09:14:53 +0100

    working remote mysql db

--- Classes/connection.php
+++ Classes/connection.php
@@ -1,27 +1,71 @@
 <?php
-    Class Connection {
-        private $server = "mysql:host=localhost;dbname=loginphp";
-        private $user = "root";
-        private $pass = "";
+    class Connection {
+        ///Applications/MAMP/tmp/mysql/mysql.sock
+        private $server = "mysql:host=remotemysql.com;dbname=BXcacvVH4F;";
+        private $user = "BXcacvVH4F";
+        private $pass = "1LmlUxGW5t";
         private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
         protected $con;
     
-        public function openConnection(){
+        public function openConnection() {
         try
             {
-        $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
-        echo "Connection to database was successful";
-        return $this->con;
+                $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
+                return $this->con;
             }
-        catch (PDOException $e)
+                catch (PDOException $e)
             {
                 echo "There is some problem in connection: " . $e->getMessage();
             }
         }
-    public function closeConnection() {
-     $this->con = null;
-  }
-}
+        public function closeConnection() {
+            $this->con = null;
+        }
+    }
+
+    // $dbhost = 'remotemysql.com:3306';
+    // $dbuser = 'BXcacvVH4F';
+    // $dbpass = '1LmlUxGW5t';
+    // $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+    
+    // if(! $conn ) {
+    //    die('Could not connect: ' . mysqli_error());
+    // }
+    // echo 'Connected successfully';
+    // mysqli_close($conn);
+
+// <?php
 
 
-?>
+// class DataBase
+// {
+
+//     private $host;
+//     private $port;
+//     private $db;
+//     private $user;
+//     private $pass;
+//     private $charset;
+//     private $dsn;
+//     private $unix_socket;
+
+
+//     public function openConnection()
+//     {
+//         $this->host = '127.0.0.1';
+//         $this->port = '3306';
+//         $this->db = 'loginphp';
+//         $this->user = 'root';
+//         $this->pass = '';
+//         $this->charset = 'utf8mb4';
+
+//         try {
+//             $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->db;charset=$this->charset";
+//             $pdo = new PDO($dsn, $this->user, $this->pass);
+//             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//             return $pdo;
+//             //Om det blir error så kan felmeddelandet visas på sidan
+//         } catch (PDOException $e) {
+//             echo "Unable to connect: ".$e->getMessage();
+//         }
+//     }
--- Classes/register.php
+++ Classes/register.php
@@ -19,36 +19,40 @@ class Register{
     public $password;
     public $email;
 
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $newConnection = new Connection();
+        $this->conn = $newConnection->openConnection();
     }
         //Register new user
-       public function register(){
-           //hashing password
-            $hash = password_hash($this->password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO users (username, password, email) VALUES ('$this->username', '$hash', '$this->email')";
+    public function register(){
+        //hashing password
+        // $hash = password_hash($this->password, PASSWORD_DEFAULT);
+        // $query = "INSERT INTO users (username) VALUES ?";
+        // $query = "INSERT INTO users (username, pass, email)  VALUES (?,?,?)";
+        // $params = array("Andreas","Andreas","Andreas");
+        // $stmt = $this->conn->prepare($query);
 
-            $stmt = $this->conn->prepare($query);
-
-             // sanitize
-            $this->username=htmlspecialchars(strip_tags($this->username));
-            $this->password=htmlspecialchars(strip_tags($this->password));
-            $this->email=htmlspecialchars(strip_tags($this->email));
-
-            $stmt->bindParam(':username', $this->username);
-            $stmt->bindParam(':password', $this->password);
-            $stmt->bindParam(':email', $this->email);
-
-            if($stmt->execute()){
-                return true;
-            }else{
-                $this->showError($stmt);
-                return false;
-            }
-        }
+            // sanitize
+        // $this->username=htmlspecialchars(strip_tags($this->username));
+        // $this->password=htmlspecialchars(strip_tags($this->password));
+        // $this->email=htmlspecialchars(strip_tags($this->email));
+        // $params = array($this->username);
+        // $stmt->bindParam(':username', $this->username);
+        // $stmt->bindParam(':password', $this->password);
+        // $stmt->bindParam(':email', $this->email);
+        $query = "SELECT * FROM users";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        print_r($data);
+        return true;
+        // $stmt->execute($params);
+        // if($stmt->execute($params)){
+        //     return true;
+        // }else{ 
+        //     $this->showError($stmt);
+        //     return false;
+        // }
+        // return true;
+    }
 }
-
-$database = new Connection();
-$db = $database->openConnection();
-
-$user = new Register($db);
