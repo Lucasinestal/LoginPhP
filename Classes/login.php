af90a817322eec041f02b5c829d2146c69ab5ce5
Kevin Fridman <kevin.fridman@yh.nackademin.se>
2020-01-30 12:05:33 +0100

    fixed session

--- Classes/login.php
+++ Classes/login.php
@@ -12,44 +12,53 @@ class Login{
 
     public function __construct($db){
         $this->conn = $db;
+      //  $this->setPassword($password);
+        //$this->setEmail($email);
 
 }       
  //Login user
+
+
 public function login(){
-   // echo("kommer in i get USER");
-   // session_start();
+     // echo ($this->password);
+     //echo ($this->email);
+    // echo"<br>";
+        // echo("kommer in i get USER");
+     // session_start();
     //$this->email = $_SESSION['email'];
-    //$this->password = $_SESSION['password'];
-
-
-    //echo($this->password);
-
-    //Hämtar rätt user från databasen
+     //$this->password = $_SESSION['password'];
+     //echo($this->password);
+        //Hämtar rätt user från databasen
     $stmt = $this->conn->prepare("SELECT * FROM users WHERE email='$this->email'");
     $stmt->execute([$this->email]); 
     $match = $stmt->fetch();
     $user = json_encode($match);
 
-    
     //check if this->password == hashed password
     $decoded = json_decode($user);
 
     $decoded->password;
     echo "<br>";
-    print_r($decoded->password);
+   // print_r($decoded->password);
     echo "<br>";
-    print_r($this->password);
+    //print_r($this->password);
     echo "<br>";
 
     if(password_verify($this->password ,$decoded->password) === true){
+        session_start();
+
+        $_SESSION["email"] = $this->email;
+
         //redirect to profile   
         echo "<br>";
         echo "correct match of passwords";
         echo "<br>";
+        header("Location: profile.php");
 
     }
     else{
         echo "wrong password";
+        header("Location: login.php");
     }
 
     
--- login.php
+++ login.php
@@ -13,14 +13,17 @@
 include_once 'Classes/connection.php';
 include_once 'Classes/login.php';
 //include_once "libs/php/utils.php";
-$loginUser= new Login($db);
 // include page header HTML
 //include_once "layout_head.php";
+$loginUser= new Login($db);
 if($_POST){
-
+    
     $database = new Connection();
     $db = $database->openConnection();
 
+    //$email = $_POST["email"];
+    //$password = $_POST["password"];
+    
     $loginUser->email=$_POST['email'];
     $loginUser->password=$_POST['password'];
     $loginUser->login();
@@ -41,24 +44,18 @@ if($_POST){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 
     <title>Login</title>
 </head>
 <body>
 
-<div class="container h-80">
-<div class="row align-items-center h-100">
-    <div class="col-3 mx-auto">
-        <div class="text-center">
-            <img id="profile-img" class="rounded-circle profile-img-card" src="https://i.imgur.com/6b6psnA.png" />
-            <p id="profile-name" class="profile-name-card"></p>
+
 
     <form action="login.php" method="POST">
-         <input type="email" name="email" placeholder="email"><br>
-         <input type="password" name="password" placeholder="password"><br>
+        <h4>Sign in</h4>
+         <input type="email" name="email" placeholder="email" required><br>
+         <input type="password" name="password" placeholder="password" required><br>
          <br>
         <input type="submit" value="submit" button class="btn btn-lg btn-primary btn-block btn-signin">
     </form>
